283cb86e37eb9159dcf6d167ca5bf9c37b15ae19
Mateusz Duma <mateuszsuplice@gmail.com>
2025-02-16 23:44:01 +0100

    initialize client application with basic structure and configuration

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { ThemeProvider } from "@/themes/theme-provider";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}

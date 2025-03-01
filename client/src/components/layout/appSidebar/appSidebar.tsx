1cfb3a830fe90f398d264a966687d36430b41322
Mateusz Duma <mateuszsuplice@gmail.com>
2025-03-01 20:21:58 +0100

    feat: enhance AppSidebar layout and add SearchCommand component for improved navigation and search functionality

--- client/src/components/layout/appSidebar/appSidebar.tsx
+++ client/src/components/layout/appSidebar/appSidebar.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import {
-  Calendar,
   Home,
   Inbox,
-  MoreHorizontal,
+  Star,
   Search,
-  Settings,
+  Trash2,
+  HardDrive,
+  Plus,
 } from "lucide-react";
 import {
   Sidebar,
@@ -13,7 +18,6 @@ import {
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenu,
-  SidebarMenuAction,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "../../ui/sidebar";
@@ -23,71 +27,82 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "../../ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
-const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
+const mainGroup = [
+  { title: "Main Page", url: "/drive", icon: Home },
+  { title: "My Drive", url: "/drive/mydrive", icon: Inbox },
+];
+
+const secondGroup = [
+  { title: "Starred", url: "/drive/starred", icon: Star },
+  { title: "Recent", url: "/drive/recent", icon: Search },
+];
+
+const thirdGroup = [
+  { title: "Trash", url: "/drive/trash", icon: Trash2 },
+  { title: "Storage", url: "/drive/storage", icon: HardDrive },
 ];
 
 const AppSidebar = () => {
+  const pathname = usePathname();
+
   return (
-    <Sidebar collapsible="icon">
+    <Sidebar
+      collapsible="none"
+      className="xl:w-64 lg:w-48 md:w-36 w-32 min-h-screen p-2"
+    >
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Filestorix</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href="#">
-                      <Home />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <SidebarMenuAction>
-                        <MoreHorizontal />
-                      </SidebarMenuAction>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent side="bottom" align="end">
-                      <DropdownMenuItem>
-                        <span>Edit Project</span>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem>
-                        <span>Delete Project</span>
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
+          <SidebarGroupLabel className="text-center font-bold ">
+            <Link href="/drive" className="lg:text-2xl text-xl">
+              Filestorix
+            </Link>
+          </SidebarGroupLabel>
         </SidebarGroup>
+        <SidebarGroup>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="primary"
+                className="w-full flex items-center gap-2"
+              >
+                <Plus size={64} />
+                <p className="text-xl font-semibold">New</p>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>Add File</DropdownMenuItem>
+              <DropdownMenuItem>Add Folder</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarGroup>
+        {[mainGroup, secondGroup, thirdGroup].map((group, idx) => (
+          <SidebarGroup key={idx}>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {group.map((item) => (
+                  <SidebarMenuItem key={item.title} title={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link href={item.url}>
+                        <item.icon />
+                        <span
+                          className={
+                            pathname === item.url
+                              ? "font-bold text-blue-500 xl:text-xl md:text-md sm:text-sm"
+                              : " xl:text-xl md:text-md sm:text-sm"
+                          }
+                        >
+                          {item.title}
+                        </span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
       </SidebarContent>
     </Sidebar>
   );
---
+++ client/src/components/layout/searchCommand/searchCommand.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useState } from "react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+
+const searchItems = [
+  "Calendar",
+  "Search Emoji",
+  "Calculator",
+  "Profile",
+  "Billing",
+  "Settings",
+];
+
+const SearchCommand = () => {
+  const [query, setQuery] = useState("");
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const filteredItems = query
+    ? searchItems
+        .filter((item) => item.toLowerCase().includes(query.toLowerCase()))
+        .slice(0, 5)
+    : [];
+
+  return (
+    <div className="relative w-full max-w-md ml-4 ">
+      <Command className="bg-muted rounded-none border-b border-gray-600 ">
+        <CommandInput
+          className="w-full border-b "
+          placeholder="Search for a file"
+          value={query}
+          onValueChange={(val) => setQuery(val || "")}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
+        {isFocused && (
+          <CommandList className="absolute top-full left-0 w-full z-50 max-w-lg bg-muted rounded-b-md ">
+            {filteredItems.length === 0 ? (
+              <CommandEmpty>No results found.</CommandEmpty>
+            ) : (
+              <CommandGroup heading="Results">
+                {filteredItems.map((item, index) => (
+                  <CommandItem key={index}>{item}</CommandItem>
+                ))}
+              </CommandGroup>
+            )}
+          </CommandList>
+        )}
+      </Command>
+    </div>
+  );
+};
+
+export default SearchCommand;

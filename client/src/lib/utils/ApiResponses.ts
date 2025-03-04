3de685dc12b22c6b2055b43ce27ab1383f354ec8
Mateusz Duma <mateuszsuplice@gmail.com>
2025-03-04 23:53:07 +0100

    feat: add new error and success messages for file upload handling

--- client/src/lib/utils/ApiResponses.ts
+++ client/src/lib/utils/ApiResponses.ts
@@ -9,6 +9,10 @@ export enum ErrorMessage {
   FAILED_GOOGLE_LOGIN = "An error occured, while trying to log in via google, please try again.",
   FAILED_GITHUB_LOGIN = "An error occured, while trying to log in via github, please try again.",
   FAILED_FETCH_FILES = "An error occured while fetching files, please try again.",
+  EXCEEDED_FILE_SIZE = "Files size exceeded maximum (10MB), please upload less files at once.",
+  FAILED_SAVING_FILES = "An error occured while trying to save files",
+  FILE_ALREADY_EXISTS = "A file with the same name already exists on your disk. Please rename the file and try again.",
+  FILE_CORRUPT_ERROR = "Saving file failed, one of your files might be corrupt, please try again.",
 }
 
 export enum SuccessMessage {
@@ -17,6 +21,7 @@ export enum SuccessMessage {
   LOGGED_OUT = "Logged out successfully!",
   SUCCESS_GOOGLE_LOGIN = "Logged in via google successfully!",
   SUCCESS_GITHUB_LOGIN = "Logged in via github successfully!",
+  SUCCESS_UPLOAD_FILES = "Uploaded files successfully!",
 }
 
 export const getErrorMessage = (errorCode?: string): string => {
--- server/internal/utils/constants/api_responses.go
+++ server/internal/utils/constants/api_responses.go
@@ -19,30 +19,59 @@ const (
 	SuccessUserGoogleLogin	= "SUCCESS_GOOGLE_LOGIN"
 	// Success message, when user successfully logged in via github
 	SuccessUserGithubLogin = "SUCCESS_GITHUB_LOGIN"
+	// Success message, when user successfuly uploads files
+	SuccessUploadFiles = "SUCCESS_UPLOAD_FILES"
 )
 
 // Api error messages
 const (
 	// Error message, when user does not exist in database
 	ErrUserExists   		= "USER_ALREADY_EXISTS" 
+
 	// Error message, when record is not found in database
 	ErrRecordNotFound 		= "INVALID_DATA"
+
 	// Error message, when unexpected error occured in database
 	ErrDBUnknown    		= "DB_UNKNOWN_ERROR"
+
 	// Error message, when provided data is invalid 
 	ErrInvalidData 			= "INVALID_DATA"
+
 	// Error message, when an unexpected error occurs
 	ErrUnexpected 			= "UNEXPECTED_ERROR"
+
 	// Error message, when user is not authorized
 	ErrUnauthorized			= "UNAUTHORIZED"
+
 	// Error message, when user session has expired
 	ErrSessionExpired		= "SESSION_EXPIRED"
+
 	// Error message, when logging in via google fails
 	ErrFaildedGoogle		= "FAILED_GOOGLE_LOGIN"
+
 	// Error message, when logging in via github fails
 	ErrFailedGithub 		= "FAILED_GITHUB_LOGIN"
+
 	// Error message, when fetching files fails
 	ErrFailedToFetchFiles 	= "FAILED_FETCH_FILES"
+
+	// Error message, when use tries to upload files 
+	// whose combined size is exceeds maximal size
+	ErrExceededFileSize 	= "EXCEEDED_FILE_SIZE"
+
+	// Error message, when saving files to database or 
+	// server folders fails
+	ErrFailedSaving 		= "FAILED_SAVING_FILES"
+
+	// Error message, when one of files to be saved
+	// already exists in database
+	ErrFileAlreadyExists 	= "FILE_ALREADY_EXISTS"
+
+	// Error message, when file gives error
+	// while trying to open fileHeader
+	// or the file is broken
+	ErrFileIsCorrupt		= "FILE_CORRUPT_ERROR"
+
 )
 
 // ParseDBError parses the provided database error based on the given context.

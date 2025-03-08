d594a083177c3d8220342f627ade16093eb0a638
Mateusz Duma <mateuszsuplice@gmail.com>
2025-03-08 17:50:47 +0100

    refactor: refactor modal handling by consolidating uploader components and removing unnecessary wrappers

--- client/src/components/layout/modalRoot/modalRoot.tsx
+++ client/src/components/layout/modalRoot/modalRoot.tsx
@@ -1,27 +1,31 @@
 "use client";
+import ModalWrapper from "@/components/sections/FileSection/ModalWrapper";
 import LoadingSpinner from "@/components/sections/loadingSpinner/loadingSpinner";
 import { useModal } from "@/hooks/use-modal";
 import { lazy, Suspense } from "react";
 
-const FileUploaderWrapper = lazy(
-  () => import("@/components/sections/FileSection/FileAdder/Wrapper")
+const FileUploader = lazy(
+  () => import("@/components/sections/FileSection/FileAdder/FileUploader")
 );
 
-const CatalogUploaderWrapper = lazy(
-  () => import("@/components/sections/FileSection/CatalogUploader/Wrapper")
+const CatalogUploader = lazy(
+  () =>
+    import("@/components/sections/FileSection/CatalogUploader/CatalogUploader")
 );
 
 const ModalRoot = () => {
   const { isOpen, modalType } = useModal();
 
+  if (!isOpen) return;
+
   return (
-    <div id="modal-root">
-      <Suspense fallback={<LoadingSpinner />}>
-        {isOpen && modalType == "addFile" && <FileUploaderWrapper />}
-      </Suspense>
-      <Suspense fallback={<LoadingSpinner />}>
-        {isOpen && modalType == "addFolder" && <CatalogUploaderWrapper />}
-      </Suspense>
+    <div>
+      <ModalWrapper>
+        <Suspense fallback={<LoadingSpinner />}>
+          {modalType == "addFile" && <FileUploader />}
+          {modalType == "addFolder" && <CatalogUploader />}
+        </Suspense>
+      </ModalWrapper>
     </div>
   );
 };

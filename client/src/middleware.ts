60d7179134c7b7b6eb8f0a9105a3f2641a79ffef
Mateusz Duma <mateuszsuplice@gmail.com>
2025-03-15 17:50:08 +0100

    feat: enhance middleware for user authentication redirects and update matcher configuration

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -6,13 +6,29 @@ export const middleware = async (req: NextRequest) => {
   const cookieStore = await cookies();
   const token = cookieStore.get("user_auth")?.value;
 
-  if (!token && req.nextUrl.pathname.startsWith("/drive")) {
+  const { pathname } = req.nextUrl;
+
+  const isLoggedIn = !!token;
+
+  if (pathname === "/") {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL("/drive", req.url));
+    } else {
+      return NextResponse.redirect(new URL("/auth/signin", req.url));
+    }
+  }
+
+  if (pathname.startsWith("/drive") && !isLoggedIn) {
     return NextResponse.redirect(new URL("/auth/signin", req.url));
+  }
+
+  if (pathname.startsWith("/auth") && isLoggedIn) {
+    return NextResponse.redirect(new URL("/drive", req.url));
   }
 
   return NextResponse.next();
 };
 
 export const config = {
-  matcher: ["/drive/:path*"],
+  matcher: ["/", "/drive/:path*", "/auth/:path*"],
 };

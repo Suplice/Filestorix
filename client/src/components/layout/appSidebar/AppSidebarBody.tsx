aa069fe214a033ca12d7a5a4ae681a288686f68c
Mateusz Duma <mateuszsuplice@gmail.com>
2025-03-18 21:05:11 +0100

    feat: add storage usage progress indicator to sidebar

--- client/src/components/layout/appSidebar/AppSidebarBody.tsx
+++ client/src/components/layout/appSidebar/AppSidebarBody.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Progress } from "@/components/ui/progress";
 import {
   SidebarGroup,
   SidebarGroupContent,
@@ -6,9 +7,12 @@ import {
   SidebarMenuItem,
   SidebarMenuButton,
 } from "@/components/ui/sidebar";
+import { useFile } from "@/hooks/use-file";
+import { formatFileSize } from "@/lib/utils/utils";
 import { HardDrive, Home, Inbox, Search, Star, Trash2 } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useMemo } from "react";
 
 const mainGroup = [
   { title: "Main Page", url: "/drive", icon: Home },
@@ -28,6 +32,23 @@ const thirdGroup = [
 const AppSidebarBody = () => {
   const pathname = usePathname();
 
+  const { files } = useFile();
+
+  const { usedStorage, totalStorage, usagePercentage } = useMemo(() => {
+    const usedStorage = files.reduce((sum, current) => sum + current.size, 0);
+    const totalStorage = 0.05 * 1024 * 1024 * 1024;
+    const usagePercentage = (usedStorage / totalStorage) * 100;
+
+    const specifiedUsedStorage = formatFileSize(usedStorage);
+    const specifiedTotalStorage = formatFileSize(totalStorage);
+
+    return {
+      usedStorage: specifiedUsedStorage,
+      totalStorage: specifiedTotalStorage,
+      usagePercentage: usagePercentage,
+    };
+  }, [files]);
+
   return (
     <>
       {[mainGroup, secondGroup, thirdGroup].map((group, idx) => (
@@ -43,7 +64,7 @@ const AppSidebarBody = () => {
                         className={
                           pathname === item.url
                             ? "font-bold text-blue-500 xl:text-xl md:text-md sm:text-sm"
-                            : " xl:text-xl md:text-md sm:text-sm"
+                            : "xl:text-xl md:text-md sm:text-sm"
                         }
                       >
                         {item.title}
@@ -52,6 +73,28 @@ const AppSidebarBody = () => {
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
+              {group.some((item) => item.title === "Storage") && (
+                <div className="px-2 py-1 select-none">
+                  <Progress
+                    value={usagePercentage}
+                    className="h-2 rounded-full"
+                    color={
+                      usagePercentage > 80
+                        ? "bg-red-500"
+                        : usagePercentage > 50
+                        ? "bg-yellow-500"
+                        : "bg-green-500"
+                    }
+                  />
+                  <p className="text-sm text-gray-500 mt-1 flex flex-row flex-wrap gap-1">
+                    <span className="font-medium text-gray-300 ">
+                      {usedStorage}
+                    </span>
+                    <span>of</span>
+                    <span className="text-gray-300">{totalStorage}</span>
+                  </p>
+                </div>
+              )}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
